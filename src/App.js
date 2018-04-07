fdea4a40d216bcab3aafd223f6bc135944e75a97
Mark De May <markdemay@Marks-iMac.PK5001Z>
2018-04-07 11:22:44 -0600

    Header/Menu bar. removed generated css.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Brewing Company</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="/beers">Beers</NavItem>
+              <NavItem eventKey={2} href="/events">Events</NavItem>
+              <NavItem eventKey={3} href="/food">Food</NavItem>
+              <NavItem eventKey={4} href="/about">About</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
